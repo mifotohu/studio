aa7276f6d85b96be3388fa7f0c8b5c27b917dbff
MIfoto.hu - a magyar AI közösség <peterprager81@gmail.com>
2026-02-21 10:23:10 +0100

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
